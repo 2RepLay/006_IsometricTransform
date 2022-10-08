a83684dc807452e28e788125aefc6d7b2392e002
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-09 01:35:00 +0300

    Initial Commit

---
+++ 006_IsometricTransform/_06_IsometricTransformApp.swift
@@ -0,0 +1,17 @@
+//
+//  _06_IsometricTransformApp.swift
+//  006_IsometricTransform
+//
+//  Created by nikita on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _06_IsometricTransformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
